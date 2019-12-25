f4d121713475cbceaa70e7bb5329f4bc636b0d3c
Muhammet Enes Ilhan <58420282+xlash5@users.noreply.github.com>
2019-12-25 09:14:09 +0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -34,29 +34,23 @@ class _QuizPageState extends State<QuizPage> {
   void checkAnswer(bool userPickedAnswer) {
     bool correctAnswer = quizbrain.getCorrectAnswer();
     setState(() {
-      //TODO: Step 4 - Use IF/ELSE to check if we've reached the end of the quiz. If so,
-      //On the next line, you can also use if (quizBrain.isFinished()) {}, it does the same thing.
+
       if (quizbrain.isFinished() == true) {
-        //TODO Step 4 Part A - show an alert using rFlutter_alert,
 
-        //This is the code for the basic alert from the docs for rFlutter Alert:
-        //Alert(context: context, title: "RFLUTTER", desc: "Flutter is awesome.").show();
-
-        //Modified for our purposes:
         Alert(
           context: context,
           title: 'Finished!',
           desc: 'You\'ve reached the end of the quiz.',
         ).show();
 
-        //TODO Step 4 Part C - reset the questionNumber,
+
         quizbrain.reset();
 
-        //TODO Step 4 Part D - empty out the scoreKeeper.
+
         scoreKeeper = [];
       }
 
-      //TODO: Step 6 - If we've not reached the end, ELSE do the answer checking steps below 👇
+
       else {
         if (userPickedAnswer == correctAnswer) {
           scoreKeeper.add(Icon(
@@ -143,8 +137,4 @@ class _QuizPageState extends State<QuizPage> {
   }
 }
 
-/*
-question1: 'You can lead a cow down stairs but not up stairs.', false,
-question2: 'Approximately one quarter of human bones are in the feet.', true,
-question3: 'A slug\'s blood is green.', true,
-*/
+
